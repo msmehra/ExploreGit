36458a57ce1c4761db658d28979dd13fff9667fe
msmehra <manpro86@gmail.com>
2021-04-11 22:48:31 +0530

    Original repo for the assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { NgxGraphModule } from '@swimlane/ngx-graph';
+import { GraphOrgTreeComponent } from './graph-org-tree/graph-org-tree.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GraphOrgTreeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxGraphModule, 
+    NgxChartsModule, 
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
